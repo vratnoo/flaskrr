de201b98f9e59a12141c1cf6ae890a2d93c95b5b
vikram ratnoo <vratnoo@gmail.com>
2019-11-11 15:29:53 +0530

    firsst commit in flask app from office computer

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import render_template
+@app.route('/')
+def hello():
+	return render_template('index.html',taa='')
+@app.route('/<kri>')	
+def hello2(kri):
+	return "HELLO,WORLD"+kri
