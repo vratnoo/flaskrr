44c265d79cc22380ca42e2aba36681d684aa09f4
vratnoo <vratnoo@gmail.com>
2019-11-12 21:57:10 +0530

    just nothin

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 @app.route('/')
 def hello():
-	return render_template('index.html',taa='what just the times',other="futkar branch")
+	return render_template('index.html',taa='what just the times',other="master branch")
 @app.route('/<kri>')	
 def hello2(kri):
 	return "HELLO,WORLD"+kri
