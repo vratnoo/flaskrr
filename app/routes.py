23254bd52fb552c72ff1bf633965351c53cd4428
vratnoo <vratnoo@gmail.com>
2019-11-12 21:12:15 +0530

    just try  to publish frrom home

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 @app.route('/')
 def hello():
-	return render_template('index.html',taa='what ever it takes')
+	return render_template('index.html',taa='what just the times')
 @app.route('/<kri>')	
 def hello2(kri):
 	return "HELLO,WORLD"+kri
