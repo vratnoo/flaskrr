1ecae4022718162734a445ec9866fca607066629
vikram ratnoo <vratnoo@gmail.com>
2019-11-13 15:30:05 +0530

    improvement from office computer

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,11 @@ from app import app
 from flask import render_template
 @app.route('/')
 def hello():
-	return render_template('index.html',taa='what just the times',other="futkar branch")
+	other = [{'author':'vikramratnoo',
+			 'book':'Homospaians'},
+			 {'author':'chetanbhagat',
+			 'book':'3 idiots'}]
+	return render_template('index.html',taa='what just the times',other=other)
 @app.route('/<kri>')	
 def hello2(kri):
 	return "HELLO,WORLD"+kri
