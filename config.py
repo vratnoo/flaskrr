632968a6e6a72e4313d6f78993afe8b94546673a
vikram ratnoo <vratnoo@gmail.com>
2019-11-15 15:10:04 +0530

    database improvement from office

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
-basdir= os.path.abspath(os.path.dirname(__file__))
+basedir= os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	"""docstring for config"""
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+	
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+ 	'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
 	SECRET_KEY=os.environ.get('SECRET_KEY') or "KRICODE"
