5a1fb6c6e6aa4a8fb25d7dc78a0ae21f677d8feb
vratnoo-home <vratnoo@gmail.com>
2019-11-17 23:31:08 +0530

    user profile added
    User Profile etc

--- app/models.py
+++ app/models.py
@@ -3,11 +3,18 @@ from datetime import datetime
 from app import login
 from werkzeug.security import check_password_hash,generate_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 @login.user_loader
 def load_user(id):
 	return User.query.get(int(id))
 
 class User(UserMixin,db.Model):
+	def avatar(self,size):
+		
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		print(digest)
+		return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(digest, size)
+
 	def set_password(self,password):
 		self.password_hash=generate_password_hash(password)
 	def check_password(self,password):
@@ -16,14 +23,16 @@ class User(UserMixin,db.Model):
 		
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
-	city = db.Column(db.String(64), index=True, unique=True)
+	
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	
 	def __repr__(self):
-		return '<User {} city {} >'.format(self.username,self.city)
+		return '<User {} about_me {} >'.format(self.username,self.about_me)
 
 class Post(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,39 @@
-from app import app
-from app import db
+from app import app,db
 from flask import render_template,flash,redirect, url_for
 from app.forms import LoginForm,RegistrationForm
 from app.models import User
-from  flask_login import current_user,login_user,logout_user
+from  flask_login import current_user,login_user,logout_user,login_required
+from hashlib import md5
+from datetime import datetime
+
+#before view function 
+@app.before_request
+def before_request():
+	if(current_user.is_authenticated):
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
+
+# User view function
+@app.route('/user/<username>')
+@login_required
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	posts = [{'author': user, 'body': 'Test post #1'},
+				  {'author': user, 'body': 'Test post #2'}]
+
+	return render_template('user.html',user=user,posts=posts)
+
+# Basic view aka Home
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	other = [{'author':'vikramratnoo',
-			 'book':'Homospaians'},
-			 {'author':'chetanbhagat',
-			 'book':'3 idiots'}]
-	return render_template('index.html',title='what just the times',other=other)
+	user = User.query.filter_by(username=current_user.username).first_or_404()
+	posts = [{'author': user, 'body': 'Test post #1'},
+				  {'author': user, 'body': 'Test post #2'}]
+	return render_template('index.html',title='what just the times',user=user,posts=posts)
+
+# login view
 @app.route('/login',methods=['GET','POST'])	
 def login():
 	if(current_user.is_authenticated):
@@ -26,11 +48,12 @@ def login():
 		flash('LOGIN REQUESSTED FOR USER {}'.format(form.username.data))
 		return redirect(url_for('index'))
 	return render_template('login.html',title="Login",form=form)
+
 @app.route('/logout')
 def logout():
 	logout_user()
 	return  redirect(url_for('index'))
-
+# Regisration page view
 @app.route('/register',methods=['GET','POST'])
 def register():
 	if(current_user.is_authenticated):
