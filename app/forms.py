392287b669895483ab1f41bb3af96d099e0ccb5a
vratnoo-home <vratnoo@gmail.com>
2019-11-21 23:04:37 +0530

    updated editing profile
    added edit profile
    duplicate user solving problem
    loggin functiionuly but not successful though

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import ValidationError,DataRequired,Email,EqualTo
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaField
+from wtforms.validators import ValidationError,DataRequired,Email,EqualTo,length
 from app.models import User
-
+from  flask_login import current_user
 class RegistrationForm(FlaskForm):
 	username= StringField('usernmae',validators=[DataRequired()])
 	email= StringField('email',validators=[Email()])
@@ -26,3 +26,19 @@ class LoginForm(FlaskForm):
 	password=PasswordField('Password',validators=[DataRequired()])
 	remember_me = BooleanField('Remember me')
 	submit= SubmitField('Log in')
+
+#for editing profile
+class EditProfileForm(FlaskForm):
+	username = StringField('Username',validators=[DataRequired()])
+	about_me = TextAreaField('About me', validators=[length(min=0,max=140)])
+	submit = SubmitField('Submit')
+	# overloaded constructor to solve duplicate username problem
+	def __init__(self, original_username, *args, **kwargs):
+		super(EditProfileForm, self).__init__(*args, **kwargs)
+		self.original_username = original_username
+	
+	def validate_username(self,username):
+		if username.data != self.original_username:
+			user = User.query.filter_by(username=username.data).first()
+			if user is not None:
+				raise ValidationError('Please use a different USER  NAME')
